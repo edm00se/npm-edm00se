babbbcc2b8db082b350c897b13e1a09c6bdb436a
Eric McCormick <edm00se@gmail.com>
2017-01-07 22:51:18 -0600

    rally fixing test function call

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const edm00se = require('../lib');
 
 describe('npm-edm00se', function(){
-  it('module should provide properties', () => {
+  it('module should provide properties', function() {
     const name = edm00se.name;
     assert.equal(name.first, 'Eric', 'first name is "Eric"');
     assert.equal(name.last, 'McCormick', 'last name is "McCormick"');
